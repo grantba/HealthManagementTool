37bd40702d339d984ed56b6bc403e4bf7b0ffc95
Blaire Grant <73077098+grantba@users.noreply.github.com>
2024-04-06 17:23:14 -0400

    Initial Commit

---
+++ HealthManagementTool/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HealthManagementTool
+//
+//  Created by Blaire Grant on 4/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HealthManagementTool/HealthManagementToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthManagementToolApp.swift
+//  HealthManagementTool
+//
+//  Created by Blaire Grant on 4/6/24.
+//
+
+import SwiftUI
+
+@main
+struct HealthManagementToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
